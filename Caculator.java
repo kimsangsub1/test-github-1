5199336e1a849aac9b946176ccb78e8c1444c7cc
songjuhong <zyechun@gmail.com>
2017-08-09 14:50:33 +0900

    add multi function

--- Caculator.java
+++ Caculator.java
@@ -19,8 +19,12 @@ public class Calculator{
   public static int minus(int a, int b){
     return a-b;
   }
-  
+
   public static int add(int a, int b){
     return a+b;
+  }
+
+  public static int multi(int a, int b){
+    return a*b;
   }
 }
