e0d75140f0769ee2feb5a0de75c9fd862045c0fa
songjuhong <zyechun@gmail.com>
2017-08-09 14:57:20 +0900

    add division

--- Caculator.java
+++ Caculator.java
@@ -19,8 +19,12 @@ public class Calculator{
   public static int minus(int a, int b){
     return a-b;
   }
-  
+
   public static int add(int a, int b){
     return a+b;
+  }
+
+  public static int division(int a, int b){
+    return a/b;
   }
 }
